fcff74ae7a7bd97a8743422d44c8841fc0f0a8f5
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2016-03-14 15:54:33 +0700

    SourceCode

---
+++ src/cetakanKue.java
@@ -0,0 +1,7 @@
+public class cetakanKue {
+    public static void main(String[] args) {
+    cetakan kue1 = new cetakan();
+    cetakan kue2 = new cetakan("Onde-Onde",250);
+    kue1.display(kue2);
+    }
+}
