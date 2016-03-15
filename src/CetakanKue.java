3a6a71667e767938a23dfa1fdcbdede70e178274
Albert Bill Alroy <albertbillalroy@gmail.com>
2016-03-15 21:31:22 +0700

    upload source code main class

---
+++ src/CetakanKue.java
@@ -0,0 +1,10 @@
+package cetakan;
+
+public class CetakanKue {
+    public static void main(String[] args) {
+        Cetakan kue = new Cetakan();
+        Cetakan kue2 = new Cetakan("Onde-onde", 250);
+        
+        kue.penjualan(kue2);
+    }
+}
