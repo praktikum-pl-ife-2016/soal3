27670c74839ee405506f013d07dee07d48fd3ab2
RizkyRamadhan <rizkyyud27@gmail.com>
2016-03-15 10:28:59 +0700

    Pengumpulan Soal 3

---
+++ src/cetakanKue.java
@@ -0,0 +1,9 @@
+
+public class cetakanKue {
+    public static void main(String[] args) {
+        cetakan kue = new cetakan();
+        cetakan kue2 = new cetakan("Onde-onde",250);
+        kue.tampilan(kue2);       
+    }
+    
+}
