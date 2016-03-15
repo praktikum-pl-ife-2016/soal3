d1d9b189f5113608fd7f74842d2434e21d56e36f
KelvinAP <kelvinaditya87@gmail.com>
2016-03-15 20:24:09 +0700

    Pengumpulan Soal 3

---
+++ src/cetakanKue.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class cetakanKue {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+cetakan kue1 = new cetakan();
+kue1.penjualan();
+cetakan kue2 = new cetakan("bolu", 200);
+kue2.penjualan();
+    
+        System.out.println("Jumlah penjualan : " + cetakan.total);
+    
+    
+}}
