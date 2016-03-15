6566d6d6f962e77a0a29f63351fb91b421886dde
nindasilvia <nindasilviatricahyani@gmail.com>
2016-03-15 09:07:40 +0700

    src code

---
+++ src/CetakanKue1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CetakanKue1 {
+  
+    public static void main(String[]args) {
+        
+        Cetakan kue = new Cetakan ();
+        Cetakan kue2 = new Cetakan  ("onde-onde",250);
+        
+        kue.tampilan(kue2);
+        
+                     
+    }
+}
+                
+        
+        // TODO code application logic here
+   
